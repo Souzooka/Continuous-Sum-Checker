74a98121cc87e1009e7fe7ba6c44f464f5633b38
Dakota <bazookaharwell@gmail.com>
2017-04-21 09:49:12 -1000

    Refactor app so that it checks for continuous series, not advancing series (n squared)

--- app.js
+++ app.js
@@ -4,20 +4,11 @@ function sumChecker(arr, num) {
   let sumTally = 0;
 
   for (let i = 0; i < arr.length; ++i) {
-    if (arr[i] > arr[i-1]) {
-      for (let j = i; j >= 0; --j) {
-        sumTally += arr[j];
-        if (arr[j] < arr[j-1]) {
-          break;
-        }
-      }
+    for (let j = i; j >= 0; --j) {
+      sumTally += arr[j];
       if (sumTally === num) {
         return true;
       }
-      sumTally = sumTally ^ sumTally;
-    }
-    else if (arr[i] === num) {
-      return true;
     }
   }
   return false;
